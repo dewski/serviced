9bf6a421c7910b5f93a0e0bdcc5da1772fa216cd
dewski <me@garrettbjerkhoel.com>
2013-06-11 12:06:48 -0700

    MongoMapper doesn't use Arel for querying

--- lib/serviced/adapters/mongo_mapper.rb
+++ lib/serviced/adapters/mongo_mapper.rb
@@ -26,6 +26,32 @@ module Serviced
 
         after_create :enqueue_refresh
       end
+
+      module ClassMethods
+        # The Class.for method handles finding or initializing new Serviced
+        # service documents for the given subject. The service document will
+        # be prefilled with values needed to save the document if one is not
+        # found.
+        #
+        # subject - The subject that the service model is associated to.
+        #
+        # Returns Serviced::Services::Model document.
+        def for(subject)
+          identifier = subject.send(identifier_column)
+          subject_id = subject.send(subject.class.primary_key)
+          subject_type = subject.class.model_name.to_s
+
+          record = find_or_initialize_by_subject_type_and_subject_id(subject_type, subject_id)
+
+          if record.persisted? && record.identifier != identifier
+            record.update_attributes(:identifier => identifier)
+          else
+            record.identifier = identifier
+          end
+
+          record
+        end
+      end
     end
   end
 end
