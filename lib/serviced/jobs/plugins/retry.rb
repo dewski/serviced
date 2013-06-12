95d6c43b5880ff1b4c0804ac61f22f3d03588600
dewski <me@garrettbjerkhoel.com>
2013-06-12 00:39:21 -0700

    Retry now has delayed_requeue

--- lib/serviced/jobs/plugins/retry.rb
+++ lib/serviced/jobs/plugins/retry.rb
@@ -16,10 +16,29 @@ module Serviced
 
             Serviced.enqueue(self, *Array(@args))
           end
+
+          # Retries the job in the future with the original arguments given to
+          # Class.perform.
+          #
+          # seconds - The amount of time in seconds to perform the job.
+          #
+          # Returns true if requeued, false if not.
+          def delayed_requeue(seconds = 60)
+            unless defined?(@args)
+              raise RuntimeError, "Attempted to requeue #{self} but there were no args present." \
+                "If you overrided Serviced::Jobs::Refresh be sure to set @args in #{self}.perform."
+            end
+
+            Resque.enqueue_in(seconds, self, *Array(@args))
+          end
         end
 
         def requeue
           self.class.requeue
+        end
+
+        def delayed_requeue(seconds = 60)
+          self.class.delayed_requeue(seconds)
         end
       end
     end
